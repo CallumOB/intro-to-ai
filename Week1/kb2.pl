3d88d945a303c2c59408cd61c773f46ce77cb8e9
CallumOB <callumobrien2004@gmail.com>
2023-09-22 09:12:53 +0100

    Lab1

---
+++ Week1/kb2.pl
@@ -0,0 +1,11 @@
+% facts
+happy(yolanda).
+listens2Music(mia).
+
+happy(tom).
+
+% rules
+listens2Music(P) :-  happy(P).
+
+playsAirGuitar(mia) :-  listens2Music(mia).
+playsAirGuitar(yolanda):-  listens2Music(yolanda). 
---
+++ Week1/likes.pl
@@ -0,0 +1,7 @@
+man(jim).
+man(mary).
+mortal(X) :- man(X).
+likes(X, A) :- man(X), dog(A).
+
+dog(rex).
+dog(lassie).
