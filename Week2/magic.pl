e0fd4eeef1e54c552b788c146d0a9f33f6bbda2d
CallumOB <callumobrien2004@gmail.com>
2023-09-29 12:17:39 +0100

    prolog

--- Week2/magic.pl
+++ Week2/magic.pl
@@ -1,7 +1,8 @@
 house_elf(dobby).
 witch(hermione).
-witch(McGonagall).
+witch('McGonagall').
 witch(rita_skeeter).
+wizard(harry).
 magic(X):-  house_elf(X).
 magic(X):-  wizard(X).
 magic(X):-  witch(X).
